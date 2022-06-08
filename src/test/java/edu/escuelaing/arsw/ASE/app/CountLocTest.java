e39c5a5a212bd89631439f5f7225ae56ab1c1c6e
Sebastian Piñeros <sebas679pb>
2022-06-08 00:35:21 -0500

    CountLocTest impl

---
+++ src/test/java/edu/escuelaing/arsw/ASE/app/CountLocTest.java
@@ -0,0 +1,55 @@
+package edu.escuelaing.arsw.ASE.app;
+
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.util.Scanner;
+
+import org.junit.*;
+
+public class CountLocTest {
+
+    @Test
+    public void deberiRealizarConteoPhyFunction() {
+        try {
+            File file = new File("src\\main\\java\\edu\\escuelaing\\arsw\\ASE\\app\\CountLoc.java");
+            Scanner scan = new Scanner(file);
+            Assert.assertEquals(102, CountLoc.phyFunction(scan));
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void deberiRealizarConteoLocFunction() {
+        try {
+            File file = new File("src\\main\\java\\edu\\escuelaing\\arsw\\ASE\\app\\CountLoc.java");
+            Scanner scan = new Scanner(file);
+            Assert.assertEquals(75, CountLoc.locFunction(scan));
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void deberiRealizarConteoPhyDirectorySearch() {
+        try {
+            File files = new File("src");
+            File[] listFiles = files.listFiles();
+            Assert.assertEquals(157, CountLoc.directorySearch("phy", listFiles));
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void deberiRealizarConteoLocDirectorySearch() {
+        try {
+            File files = new File("src");
+            File[] listFiles = files.listFiles();
+            Assert.assertEquals(122, CountLoc.directorySearch("loc", listFiles));
+        } catch (Exception e) {
+            fail();
+        }
+    }
+}
